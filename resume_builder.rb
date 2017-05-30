1b28425a412f923347e5b954323042c2574d7cf9
Akhil Gauatm <akhilg@mindfiresolutions.com>
2017-05-30 15:29:06 +0530

    moved orphan codes to a method and changes in validation module

--- resume_builder.rb
+++ resume_builder.rb
@@ -1,7 +1,9 @@
 require "./validate.rb"
-class Resume_builder
+class ResumeBuilder
 	include Validate
+
 	attr_accessor :first_name, :last_name, :dob, :work_experience, :work_description, :email_id
+	
 	def save_as_file
 		f = File.open("#{@first_name}.txt", "w+")
 		f.puts "First Name: #{@first_name}"
@@ -11,56 +13,44 @@ class Resume_builder
 		f.puts "Work Description: #{work_description}"
 		f.puts "E-mail id: #{email_id}"
 	end
-	def input_first_name
-		puts "Enter first name(should be more than 2 characters):"
-		@first_name = gets.chomp
+
+	def self.start
+		person = ResumeBuilder.new()
+		person.input_function_calls
+		person.save_as_file
+		puts "You resume is ready for download!"
 	end
-	def input_last_name
-		puts "Enter second name(should be more than 2 characters):"
-		@last_name = gets.chomp
+
+	def input_function_calls
+		begin
+			puts "Enter first name(should be more than 2 characters):"
+			@first_name = gets.chomp
+		end while self.valid_fname
+
+		begin
+			puts "Enter second name(should be more than 2 characters):"
+			@last_name = gets.chomp
+		end while self.valid_lname
+
+		begin
+			puts "Enter your date of birth (e.g. DD-MM-YYYY): "
+			@dob = gets.chomp
+		end while self.valid_date
+
+		begin
+			puts "Enter your work experience in years:"
+			@work_experience = gets.chomp
+		end while self.valid_experience
+
+		begin
+			puts "Enter your work description in not more than 10 words:"
+			@work_description = gets.chomp
+		end while self.valid_description
+
+		begin
+			puts "Enter your Email ID(e.g abc@xyz.com):"
+			@email_id = gets.chomp
+		end while self.valid_email
 	end
-	def date_of_birth
-		puts "Enter your date of birth (e.g. DD-MM-YYYY): "
-		@dob = gets.chomp
-	end
-	def work_exp
-		puts "Enter your work experience in years:"
-		@work_experience = gets.chomp
-	end
-	def work_descrip
-		puts "Enter your work description in not more than 10 words:"
-		@work_description = gets.chomp
-	end
-	def email
-		puts "Enter your Email ID(e.g abc@xyz.com):"
-		@email_id = gets.chomp
-	end
-	person = Resume_builder.new()
-
-	begin
-		person.input_first_name
-	end while person.valid_fname
-
-	begin
-		person.input_last_name
-	end while person.valid_lname
-
-	begin
-		person.date_of_birth
-	end while person.valid_date
-
-	begin
-		person.work_exp
-	end while person.valid_experience
-
-	begin
-		person.work_descrip
-	end while person.valid_description
-
-	begin
-		person.email
-	end while person.valid_email
-
-	person.save_as_file
-	puts "You resume is ready for download!"
 end
+ResumeBuilder.start
--- validate.rb
+++ validate.rb
@@ -1,7 +1,9 @@
 require 'date'
 module Validate
+
+	#a valid last name should be more than 2 chars long and should not be empty
 	def valid_fname
-		if (@first_name.length) < 3
+		if ((@first_name.length) < 3) || (@first_name.split(' ').size == 0)
 			puts "Sorry! The first name invalid. Try again."
 			return true
 		else
@@ -9,8 +11,9 @@ module Validate
 		end 
 	end
 
+	#a valid last name should be more than 2 chars long and should not be empty
 	def valid_lname
-		if (@last_name.length) < 3
+		if (@last_name.length) < 3 || (@last_name.split(' ').size == 0)
 			puts "Sorry! The last name is invalid. Try again."
 			return true
 		else
@@ -18,6 +21,7 @@ module Validate
 		end 
 	end
 
+	#a valid date of birth is one in the format DD-MM-YYYY and less than current date
 	def valid_date
 		begin
 			x = Date::strptime(@dob, "%d-%m-%Y")
@@ -25,7 +29,7 @@ module Validate
 			puts "Sorry! You have entered date in an invalid format."
 			return true
 		end
-		if (x.year >= Date.today.year)
+		if (x >= Date.today)
 			puts "Sorry! You age is invalid. Try again."
 			return true
 		else
@@ -33,13 +37,17 @@ module Validate
 		end
 	end
 
+	#a valid experience duration should not exceed the age
 	def valid_experience
-		if ((Date.today.year - Date::strptime(@dob, "%d-%m-%Y").year)).to_i <= @work_experience.to_i
+		if ((Date.today.year - Date::strptime(@dob, "%d-%m-%Y").year)) < @work_experience.to_i
 			puts "Sorry! Work experience can't exceed your age. Try again."
 			return true
+		else
+			return false
 		end
 	end
 
+	#check if the number of words is more than 10
 	def valid_description
 		if @work_description.split(' ').size > 10
 			puts "Sorry! Try to describe in 10 words only."
@@ -49,7 +57,8 @@ module Validate
 		end
 	end
 
-	def valid_email
+	#email verification needs to have RegExr matching
+	def valid_email					
 		if @email_id.size < 5
 			puts "Sorry! Invalid E-Mail ID. Try again."
 			return true
